b95b994d82f6a439e1f5f4d14036622d1b43f5b8
kmjanith <kavinduj.20@cse.mrt.ac.lk>
2024-12-17 12:25:58 +0530

    first commit

---
+++ kavindu-janith/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header className='fixed top-0 left-0 w-fullh h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0'>
+        <div className='max-w-screen-2xl w0full mx-auto px-4'>
+
+            <h1></h1>
+        </div>
+    </header>
+  )
+}
